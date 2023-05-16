109edefdfcc2779ba59a57de8cc6253b037cbd96
Sana Assim <sanaassim1@gmail.com>
2023-05-16 09:07:13 -0700

    Create a new variable to add to the env if setenv called

--- more-set_env.c
+++ more-set_env.c
@@ -36,3 +36,39 @@ char **allocate_sp(char **env)
 
 	return (new_env);
 }
+
+/**
+ * new_varToAdd - Create a new environment variable string to add it to the env
+ *
+ * @name: The name of the variable
+ * @value: The value of the variable
+ *
+ * Return: The new environment variable string on success
+ */
+char *new_varToAdd(char *name, char *value)
+{
+	char *varToAdd;
+	int n, m;
+
+	varToAdd = malloc(sizeof(char) * (own_strlen(name) + own_strlen(value) + 2));
+	if (varToAdd == NULL)
+		return (NULL);
+
+	n = 0;
+	while (name[n])
+	{
+		varToAdd[n] = name[n];
+		n++;
+	}
+	varToAdd[n++] = '=';
+
+	m = 0;
+	while (value[m])
+	{
+		varToAdd[n++] = value[m];
+		m++;
+	}
+	varToAdd[n++] = '\0';
+
+	return (varToAdd);
+}
